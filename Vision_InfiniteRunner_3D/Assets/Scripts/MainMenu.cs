4dff2efb32d8032e587975b9013305d3b77a12e7
Sinanf <sinanfozdemir@gmail.com>
2022-03-27 19:04:43 +0300

    trying to save toggle data with music

--- Vision_InfiniteRunner_3D/Assets/Scripts/MainMenu.cs
+++ Vision_InfiniteRunner_3D/Assets/Scripts/MainMenu.cs
@@ -2,10 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class MainMenu : MonoBehaviour
 {
+    public Toggle toggle;    
+    private bool toggleOn;
+
+    private void Start()
+    {
+        
+        toggle.isOn = toggleOn;
+        ToggleBool(toggleOn);
+        
+        
+        
+        
+        
+
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
@@ -15,5 +32,41 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    
+    public void CheckData()
+    {
+        if (PlayerPrefs.HasKey("MusicData"))
+        {
+            
+            if (PlayerPrefs.GetInt("MusicData") > 0)
+            {
+                toggleOn = true;
+            }
+            else
+            {
+                toggleOn = false;
+            }
+        }
+        PlayerPrefs.Save();
+        
+    }
+
+    
+
+    public void ToggleBool (bool toggleOn)
+    {
+        if (toggleOn == true)
+        {
+            PlayerPrefs.SetInt("MusicData", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt("MusicData", 0);
+        }
+        CheckData();
+    }
+
+
 
 }
